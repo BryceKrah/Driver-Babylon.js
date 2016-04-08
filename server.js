a5c279b3c033e10a1f6975e9d049920300f46d17
Bryce Krah <krahbryce@yahoo.com>
2016-04-08 14:02:05 -0400

    syntax error fix

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ if(process.env.ENVIRONMENT === 'production'){
     user: process.env.DB_USER,
     password: process.env.DB_PASS
 }
+}
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));
