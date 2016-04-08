a133a7309e8a110a074bb9cacac095dfca65581b
Bryce Krah <krahbryce@yahoo.com>
2016-04-08 16:26:57 -0400

    cn fix

--- db/pgp.js
+++ db/pgp.js
@@ -5,13 +5,7 @@ require('dotenv').config()
 if(process.env.ENVIRONMENT === 'production'){
   var cn = process.env.DATABASE_URL;
 } else {
-  var cn = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS
-  };
+  var cn = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@localhost/${process.env.DB_NAME}`
 }
 
 
